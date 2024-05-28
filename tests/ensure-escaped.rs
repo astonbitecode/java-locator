9af3cc5fc251d770189e6b61ac33b9f0c9cf12fb
Russell Cohen <rcoh@amazon.com>
2024-05-28 15:01:36 +0000

    Fix pathing for windows by cfg-gating test

--- tests/ensure-escaped.rs
+++ tests/ensure-escaped.rs
@@ -1,11 +1,27 @@
 use java_locator::locate_jvm_dyn_library;
 
+// Windows does not support `[`, `]`, or `*` in paths so this test does not apply
+#[cfg(not(target_os = "windows"))]
 #[test]
 fn test_javahome_can_be_escaped() {
-    println!("{:?}", std::env::current_dir());
-    std::env::set_var("JAVA_HOME", "tests/[funky-javahome]/nested");
+    use std::env::temp_dir;
+
+    let test_path = temp_dir()
+        .join("[funky-javahome]")
+        .join("nested")
+        .join("*dir*");
+
+    std::fs::create_dir_all(&test_path).expect("failed to create directory");
+    std::fs::write(test_path.join("libjvm.so"), "stub-file").unwrap();
+    std::env::set_var(
+        "JAVA_HOME",
+        test_path.to_str().expect("no invalid characters"),
+    );
     assert_eq!(
         locate_jvm_dyn_library().expect("failed to located jvm library"),
-        "tests/[funky-javahome]/nested/more*nested"
+        format!(
+            "{}",
+            temp_dir().join("[funky-javahome]/nested/*dir*").display()
+        )
     );
 }
